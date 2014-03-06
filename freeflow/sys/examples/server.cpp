e3c851fa72998cf7191e1ffb8f4492f5673fa90f
Andrew Sutton <andrew.n.sutton@gmail.com>
2014-03-06 07:44:21 -0500

    Rewriting string comparisons in example.

--- freeflow/sys/examples/server.cpp
+++ freeflow/sys/examples/server.cpp
@@ -4,6 +4,8 @@
 
 #include <freeflow/sys/socket.hpp>
 
+using namespace freeflow;
+
 int main(int argc, char** argv) {
   if(argc != 5) {
     std::cerr << "usage syntax: " << argv[0] 
@@ -11,37 +13,40 @@ int main(int argc, char** argv) {
     std::exit(-1);
   }
 
-  std::string ipv4("ipv4");
-  std::string ipv6("ipv6");
-  std::string tcp("tcp");
-  std::string udp("udp");
+  std::string ipv4 = "ipv4";
+  std::string ipv6 = "ipv6";
+  std::string tcp = "tcp";
+  std::string udp = "udp";
 
-  using namespace freeflow;
-
-  Socket::Transport t;
-  if (tcp.compare(argv[4]) == 0)
-    t = Socket::TCP;
-  else if (tcp.compare(argv[4]) == 0)
-    t = Socket::UDP;
-
+  // Select the address family.
   Address::Family net;
-  if (ipv4.compare(argv[1]) == 0)
+  if (argv[1] == ipv4)
     net = Address::IPv4;
-  else if (ipv4.compare(argv[1]) == 0)
+  else if (argv[1] == ipv6)
     net = Address::IPv6;
 
-  uint16_t port = atoi(argv[3]);
-  
-  // try {
+  // Get the addres and port.
+  std::string addr = argv[2];
+  Ip_port port = atoi(argv[3]);
+
+  // Select the transpot
+  Socket::Transport t;
+  if (argv[4] == tcp)
+    t = Socket::TCP;
+  else if (argv[4] == udp)
+    t = Socket::UDP;
+
+
+  try {
     Socket server(net, t);
-    server.bind(Address(net, argv[2], port));
+    server.bind(Address(net, addr, port));
     if (t == Socket::TCP) {
       server.listen();
       std::cout << "Listening: " << to_string(server.local) << std::endl ;
       Socket child = server.accept();
     } else if (t == Socket::UDP) {
     }
-  // } catch(Error e) {
-  //   std::cerr << "Error: " << strerror(e.code()) << std::endl;
-  // }
+  } catch(Error e) {
+    ::perror("Error");
+  }
 }
