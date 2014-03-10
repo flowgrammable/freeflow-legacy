f602bbfb394bbd4e7ac26a91d26c9c23cb9c5989
Andrew Sutton <andrew.n.sutton@gmail.com>
2014-03-10 11:07:37 -0400

    Adding missing file.

---
+++ freeflow/proto/ofp/v1.0/error.hpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2013-2014 Flowgrammable.org
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an "AS IS"
+// BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+#ifndef FREEFLOW_OFPV1_0_ERROR_HPP
+#define FREEFLOW_OFPV1_0_ERROR_HPP
+
+#include <freeflow/sys/error.hpp>
+
+namespace freeflow {
+namespace ofp {
+namespace v1_0 {
+
+// Note that this class is named "Errc" (for error code) in this 
+// namespace to avoid collisions with the Error message.
+struct Errc : ofp::Error {
+  /// The message type is unsupported
+  static constexpr Code BAD_MESSAGE_TYPE = 200;
+
+  static constexpr Code HELLO_OVERFLOW   = 201;
+  static constexpr Code ERROR_OVERFLOW   = 202;
+  static constexpr Code ECHO_OVERFLOW    = 203;
+  static constexpr Code VENDOR_OVERFLOW  = 204;
+  static constexpr Code FEATURE_OVERFLOW = 205;
+  static constexpr Code CONFIG_OVERFLOW  = 206;
+
+  using ofp::Error::Error;
+};
+
+} // namespace v1_0
+} // namespace ofp
+} // namespace freeflow
+
+#endif
