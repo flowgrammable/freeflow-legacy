40202a326b16a7a3f044038639d3accdff3d4332
Jasson Casey <jasson@flowgrammable.com>
2014-04-22 09:36:23 -0400

    Playing with trie

--- freeflow/alg/trie.hpp
+++ freeflow/alg/trie.hpp
@@ -2,6 +2,26 @@
 #define FREEFLOW_TRIE_HPP
 
 namespace freeflow {
+
+struct Key
+{
+  uint8_t value;
+  uint8_t mask;
+}
+
+template<std::size_t byte>
+  class Trie
+  {
+
+    Trie();
+    ~Trie();
+
+    void insert(const &key[T]);
+    void remove();
+    bool lookup();
+    
+  };
+
 }
 
 #include "trie.ipp"
