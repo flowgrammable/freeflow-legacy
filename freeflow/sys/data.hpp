c2d95e880d280dd1d4689667a58a490372f47dea
Andrew Sutton <andrew.n.sutton@gmail.com>
2014-04-24 09:47:53 -0400

    Adding aliases for signed integer types.

--- freeflow/sys/data.hpp
+++ freeflow/sys/data.hpp
@@ -24,6 +24,11 @@ namespace freeflow {
 // -------------------------------------------------------------------------- //
 // Data types
 
+using Int8 = std::int8_t;
+using Int16 = std::int16_t;
+using Int32 = std::int32_t;
+using Int64 = std::int64_t;
+
 using Uint8 = std::uint8_t;
 using Uint16 = std::uint16_t;
 using Uint32 = std::uint32_t;
