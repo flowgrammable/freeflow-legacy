ab1a68cba99d0e586d99b96afad9003197849435
Thomas Wynn <twynn3348@gmail.com>
2014-05-20 10:58:28 -0400

    Stubbed out some of the app infrastructure

---
+++ apps/noflow/noflow.hpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2013-2014 Flowgrammable.org
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an "AS IS"
+// BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+#ifndef NOCONTROL_NOFLOW_HPP
+#define NOCONTROL_NOWFLOW_HPP
+
+#include <freeflow/nbi/application.hpp>
+#include <freeflow/nbi/controller.hpp>
+#include <freeflow/nbi/switch.hpp>
+
+#include <nocontrol/config.hpp>
+
+namespace nocontrol {
+
+/// The noflow 
+class Noflow : public ff::Application {
+  enum Stop { ON_BIND, ON_VERSION, ON_FEATURE };
+public:
+  void load(ff::Controller&);
+  void bind(ff::Switch&);
+  void version_known(ff::Switch&);
+  void features_known(ff::Switch&);
+
+private:
+  Stop stop_ = ON_BIND; // The stopping point.
+};
+
+} // namespace nocontrol
+
+#endif
